48d51cac0f2ff8a3b7ef9ad0a09fa430fc043b9a
guillaume-ventura <venturaguillaume01@gmail.com>
2022-11-29 12:03:08 +0100

    ➕ Add AirBNB eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,17 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: ["@remix-run/eslint-config", "@remix-run/eslint-config/node"],
+  extends: ['@remix-run/eslint-config', '@remix-run/eslint-config/node', 'airbnb'],
+  overrides: [
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      extends: ['airbnb-typescript'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
 };
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -4,5 +4,5 @@ export default function Index() {
     <div className="h-screen bg-slate-700 flex justify-center items-center">
       <h2 className="text-blue-600 font-extrabold text-5xl">TailwindCSS Is Working!</h2>
     </div>
-  )
+  );
 }
