6885cc71707e1c9c84a4bc0028cb6effe6b22aae
Julien Millaud <millaud.julien@gmail.com>
2022-11-29 10:49:14 +0100

    :white_check_mark: add tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,9 @@
 import type { MetaFunction } from "@remix-run/node";
+import styles from "./styles/app.css"
+
+export function links() {
+  return [{ rel: "stylesheet", href: styles }]
+}
 import {
   Links,
   LiveReload,
