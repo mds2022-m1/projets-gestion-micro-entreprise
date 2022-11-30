512a8145cfae9cb5738a08692987cd656b5ec1f0
Léo <leomarchal.dev@gmail.com>
2022-11-30 16:32:54 +0100

    ✨ Add sidebar

--- app/root.tsx
+++ app/root.tsx
@@ -7,6 +7,7 @@ import {
   Scripts,
   ScrollRestoration,
 } from '@remix-run/react';
+import { Sidebar } from './components/Sidebar';
 import styles from './tailwind.css';
 
 export function links() {
@@ -27,6 +28,7 @@ export default function App() {
         <Links />
       </head>
       <body>
+        <Sidebar />
         <Outlet />
         <ScrollRestoration />
         <Scripts />
