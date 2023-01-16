1ad0448ece27a895e99eb1f7e4e59a5f6e4939df
JulienNirxo <millaud.julien@gmail.com>
2023-01-16 15:40:45 +0100

    ✨ Add modal organization typ

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,8 +1,20 @@
-// ./app/routes/index.tsx
+import React, { useState } from 'react';
+import { ModalOrganizationType } from '../components/ModalOrganizationType';
+
 export default function Index() {
-  return (
-    <div className="lg:ml-64 h-screen bg-slate-700 flex flex-col justify-center items-center">
-      <h2 className="text-blue-600 font-extrabold text-5xl">TailwindCSS Is Working!</h2>
-    </div>
-  );
+    const [showModal, setShowModal] = useState(false);
+
+    function Index(bool: boolean) {
+      setShowModal(false)
+      setTimeout(() => {
+        setShowModal(true)
+      }, 500)
+    }
+
+    return (
+        <div className="lg:ml-64 h-screen bg-slate-700 flex flex-col justify-center items-center">
+             <button onClick={() => Index(true)} className="bg-blue-500 text-white px-4 py-2 rounded-lg">Open Modal</button>
+             {showModal && <ModalOrganizationType/>}
+        </div>
+    );
 }
